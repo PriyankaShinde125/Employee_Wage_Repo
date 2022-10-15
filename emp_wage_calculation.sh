a5c90d8c172ee88b324ddc9e100bebdffb07789b
“PriyankaShinde” <pkshinde125@gmail.com>
2022-10-16 00:44:17 +0530

    [Priyanka] integrated with UC2 daily wage calculation

--- emp_wage_calculation.sh
+++ emp_wage_calculation.sh
@@ -2,12 +2,18 @@
 
 echo " Welcome to Employee Wage Computation"
 
-
+perHourSalary=20
+fullDayHours=8
 attendance=$((RANDOM%2))
 isPresent=1
-if [ $isPresent -eq $attendance ]
+workHours=0
+salPerDay=0
+if [ $attendance -eq $isPresent ]
 then
 echo "Present"
+workHours=8
 else
 echo "Absent"
 fi
+
+echo "Todays earning=" $(($workHours*$perHourSalary)) 
