aedb2cb085bd1f8fb995750bb56b827b30e494f8
“PriyankaShinde” <pkshinde125@gmail.com>
2022-10-16 01:19:46 +0530

    [Priyanka] integrated with UC5 wage calculation for month

--- emp_wage_calculation.sh
+++ emp_wage_calculation.sh
@@ -5,14 +5,23 @@ echo " Welcome to Employee Wage Computation"
 perHourSalary=20
 fullDayHours=8
 partTimeHours=4
-attendance=$((RANDOM%3))
+
 workHours=0
-salPerDay=0
+totalSalary=0
+perDaySalary=0
+
+for ((day=1; day<=20 ;day++))
+do
+ attendance=$((RANDOM%3))
 case $attendance in
-	0) echo "absent";;
+	0) echo "absent"
+		workHours=0;;
 	1) echo "Present"
 		workHours=8;;
 	2) echo "part time present"
 		workHours=4;;
 esac
-echo "Todays earning=" $(($workHours*$perHourSalary)) 
+perDaySalary=$(($workHours*$perHourSalary))
+totalSalary=$(($totalSalary+$perDaySalary))
+done
+echo "Month earning=" $totalSalary 
